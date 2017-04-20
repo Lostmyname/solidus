88de40feccba86ef9dbbdd4b37cb5a4a8b87af9a
Vlad Stoica <vladstoick@users.noreply.github.com>
2017-04-20 18:44:18 +0100

    Merge pull request #15 from Lostmyname/tax-mm-improv
    Tax Category - Tax Rate M-M improvements

--- core/app/models/spree/tax_rate_tax_category.rb
+++ core/app/models/spree/tax_rate_tax_category.rb
@@ -1,7 +1,5 @@
 module Spree
-  class TaxRateCategory < Spree::Base
-    self.table_name = "spree_tax_rates_categories"
-
+  class TaxRateTaxCategory < Spree::Base
     belongs_to :tax_rate, class_name: Spree::TaxRate
     belongs_to :tax_category, class_name: Spree::TaxCategory
   end
